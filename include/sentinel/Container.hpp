32394f7820fbb3fa7b9787c74e6d4e67416bcf9e
Sung Gon Kim <sunggon82.kim@lge.com>
2020-09-18 11:08:40 +0900

    #65 Adds common template interfaces and classes

---
+++ include/sentinel/Container.hpp
@@ -0,0 +1,288 @@
+/*
+  MIT License
+
+  Copyright (c) 2020 Sung Gon Kim
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef INCLUDE_SENTINEL_CONTAINER_HPP_
+#define INCLUDE_SENTINEL_CONTAINER_HPP_
+
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <random>
+#include <vector>
+
+
+namespace sentinel {
+
+/**
+ * @brief Container template class
+ *
+ * @tparam T object type
+ */
+template <typename T>
+class Container {
+ public:
+  /**
+   * @brief Default constructor
+   */
+  Container() = default;
+
+  /**
+   * @brief Default constructor
+   *
+   * @param first iterator
+   * @param last iterator
+   */
+  Container(typename std::vector<T>::iterator first,
+            typename std::vector<T>::iterator last);
+
+  /**
+   * @brief Return the number of elements
+   *
+   * @return size
+   */
+  std::size_t size() const noexcept;
+
+  /**
+   * @brief Return the iterator pointing to the first element
+   *
+   * @return the beginning iterator
+   */
+  typename std::vector<T>::iterator begin() noexcept;
+
+  /**
+   * @brief Return the iterator pointing to the first element
+   *
+   * @return the beginning iterator
+   */
+  typename std::vector<T>::const_iterator begin() const noexcept;
+
+  /**
+   * @brief Return the iterator pointing to the past-the-end element
+   *
+   * @return the past-the-end iterator
+   */
+  typename std::vector<T>::iterator end() noexcept;
+
+  /**
+   * @brief Return the iterator pointing to the past-the-end element
+   *
+   * @return the past-the-end iterator
+   */
+  typename std::vector<T>::const_iterator end() const noexcept;
+
+  /**
+   * @brief Returns a reference to the element at the given position
+   *
+   * @param position of an element
+   * @return reference of the element
+   */
+  typename std::vector<T>::reference at(std::size_t position);
+
+  /**
+   * @brief Returns a reference to the element at the given position
+   *
+   * @param position of an element
+   * @return reference of the element
+   */
+  typename std::vector<T>::const_reference at(std::size_t position) const;
+
+  /**
+   * @brief Returns a reference to the element at the given position
+   *
+   * @param position of an element
+   * @return reference of the element
+   */
+  typename std::vector<T>::reference operator[](std::size_t position);
+
+  /**
+   * @brief Returns a reference to the element at the given position
+   *
+   * @param position of an element
+   * @return reference of the element
+   */
+  typename std::vector<T>::const_reference operator[](std::size_t position) const;
+
+  /**
+   * @brief Add the element at the end
+   *
+   * @param object to add
+   */
+  void push_back(const typename std::vector<T>::value_type& object);
+
+  /**
+   * @brief Add the element at the end
+   *
+   * @param object to add
+   */
+  void push_back(typename std::vector<T>::value_type&& object);
+
+  /**
+   * @brief Construct and add the end
+   *
+   * @tparam Args constructor argument types
+   * @param args for constructor
+   */
+  template <class... Args>
+  void emplace_back(Args&&... args);
+
+  /**
+   * @brief Sort the elements into ascending order
+   */
+  void sort();
+
+  /**
+   * @brief Shuffle the elements
+   */
+  void shuffle();
+
+  /**
+   * @brief Clear the elements
+   */
+  void clear() noexcept;
+
+  /**
+   * @brief Save the elements to the file
+   *
+   * @param path to file
+   */
+  virtual void save(const std::string& path);
+
+  /**
+   * @brief Load elements from the file
+   *
+   * @param path to file
+   */
+  virtual void load(const std::string& path);
+
+ private:
+  std::vector<T> mData;
+};
+
+template <typename T>
+Container<T>::Container(typename std::vector<T>::iterator first,
+                        typename std::vector<T>::iterator last) :
+    mData(first, last) {
+}
+
+template <typename T>
+std::size_t Container<T>::size() const noexcept {
+  return mData.size();
+}
+
+template <typename T>
+typename std::vector<T>::iterator Container<T>::begin() noexcept {
+  return mData.begin();
+}
+
+template <typename T>
+typename std::vector<T>::const_iterator Container<T>::begin() const noexcept {
+  return mData.begin();
+}
+
+template <typename T>
+typename std::vector<T>::iterator Container<T>::end() noexcept {
+  return mData.end();
+}
+
+template <typename T>
+typename std::vector<T>::const_iterator Container<T>::end() const noexcept {
+  return mData.end();
+}
+
+template <typename T>
+typename std::vector<T>::reference Container<T>::at(std::size_t position) {
+  return mData.at(position);
+}
+
+template <typename T>
+typename std::vector<T>::const_reference Container<T>::at(std::size_t position) const {
+  return mData.at(position);
+}
+
+template <typename T>
+typename std::vector<T>::reference
+Container<T>::operator[](std::size_t position) {
+  return mData.at(position);
+}
+
+template <typename T>
+typename std::vector<T>::const_reference
+Container<T>::operator[](std::size_t position) const {
+  return mData.at(position);
+}
+
+template <typename T>
+void Container<T>::push_back(const typename std::vector<T>::value_type& object) {
+  mData.push_back(object);
+}
+
+template <typename T>
+void Container<T>::push_back(typename std::vector<T>::value_type&& object) {
+  mData.push_back(object);
+}
+
+template <typename T>
+template <class... Args>
+void Container<T>::emplace_back(Args&&... args) {
+  mData.emplace_back(args...);
+}
+
+template <typename T>
+void Container<T>::sort() {
+  std::sort(mData.begin(), mData.end());
+}
+
+template <typename T>
+void Container<T>::shuffle() {
+  std::shuffle(mData.begin(), mData.end(),
+               std::mt19937 { std::random_device{}() });
+}
+
+template <typename T>
+void Container<T>::clear() noexcept {
+  mData.clear();
+}
+
+template <typename T>
+void Container<T>::save(const std::string& path) {
+  std::ofstream ofs(path);
+  for (const auto& data : mData) {
+    ofs << data << std::endl;
+  }
+  ofs.close();
+}
+
+template <typename T>
+void Container<T>::load(const std::string& path) {
+  T object;
+  std::ifstream ifs(path);
+  while (ifs >> object) {
+    mData.push_back(object);
+  }
+  ifs.close();
+}
+
+}  // namespace sentinel
+
+#endif  // INCLUDE_SENTINEL_CONTAINER_HPP_
---
+++ test/ContainerTest.cpp
@@ -0,0 +1,125 @@
+/*
+  MIT License
+
+  Copyright (c) 2020 Sung Gon Kim
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+#include "sentinel/util/os.hpp"
+#include "sentinel/Container.hpp"
+
+
+namespace sentinel {
+
+class Integers : public Container<int> {
+ public:
+  bool operator==(const Integers& other) const {
+    return std::equal(begin(), end(), other.begin());
+  }
+
+  bool operator!=(const Integers& other) const {
+    return !std::equal(begin(), end(), other.begin());
+  }
+};
+
+class ContainerTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    for (auto i = 0; i < N; i++) {
+      integers.push_back(i);
+    }
+  }
+
+  Integers integers;
+  Integers none;
+  const std::size_t N = 10;
+};
+
+TEST_F(ContainerTest, testSize) {
+  EXPECT_EQ(0, none.size());
+  EXPECT_EQ(N, integers.size());
+}
+
+TEST_F(ContainerTest, testIterators) {
+  EXPECT_EQ(none.begin(), none.end());
+  EXPECT_EQ(N, std::distance(integers.begin(), integers.end()));
+}
+
+TEST_F(ContainerTest, testAt) {
+  EXPECT_THROW(none.at(0), std::out_of_range);
+  EXPECT_THROW(none.at(N), std::out_of_range);
+  EXPECT_EQ(0, integers.at(0));
+  EXPECT_EQ(N - 1, integers.at(N - 1));
+  EXPECT_THROW(integers.at(N), std::out_of_range);
+}
+
+TEST_F(ContainerTest, testSubscriptOperator) {
+  EXPECT_THROW(none[0], std::out_of_range);
+  EXPECT_THROW(none[N], std::out_of_range);
+  EXPECT_EQ(0, integers[0]);
+  EXPECT_EQ(N - 1, integers[N - 1]);
+  EXPECT_THROW(integers[N], std::out_of_range);
+}
+
+TEST_F(ContainerTest, testPushback) {
+  none.push_back(1);
+  none.push_back(1);
+  EXPECT_EQ(2, none.size());
+}
+
+TEST_F(ContainerTest, testEmplaceback) {
+  none.emplace_back(1);
+  none.emplace_back(1);
+  EXPECT_EQ(2, none.size());
+}
+
+TEST_F(ContainerTest, testSort) {
+  none.emplace_back(2);
+  none.emplace_back(1);
+  none.sort();
+  EXPECT_EQ(1, none[0]);
+  EXPECT_EQ(2, none[1]);
+}
+
+TEST_F(ContainerTest, testShuffle) {
+  auto shuffled = integers;
+  shuffled.shuffle();
+  EXPECT_NE(integers, shuffled);
+}
+
+TEST_F(ContainerTest, testClear) {
+  EXPECT_EQ(N, integers.size());
+  integers.clear();
+  EXPECT_EQ(0, integers.size());
+}
+
+TEST_F(ContainerTest, testSaveAndLoad) {
+  auto path = os::tempPath();
+  integers.save(path);
+  none.load(path);
+  EXPECT_EQ(integers, none);
+  os::removeFile(path);
+}
+
+}  // namespace sentinel
