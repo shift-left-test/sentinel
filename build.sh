0a30b0c69bb8da1cd6574725aa1fe646798cae15
sunggon82.kim <sunggon82.kim@lge.com>
2026-04-04 22:25:36 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 cmake . -DCMAKE_TESTING_ENABLED=ON
 make cppcheck cpplint doc
 make -j
+find . -name "*.gcda" -delete
 ctest --output-on-failure -j$(nproc)
 gcovr -s -r . --object-directory .
 make package
